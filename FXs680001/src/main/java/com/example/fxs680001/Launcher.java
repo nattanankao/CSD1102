5cf1ec85063b7610d3bc1b8e1993dfc47925976d
nattanankao <s68122250001@ssru.ac.th>
2026-03-17 21:31:18 +0700

    Delete FXs680001 directory

--- FXs680001/src/main/java/com/example/fxs680001/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.fxs680001;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(Gui3Application.class, args);
-    }
-}
--- FXs680001/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.fxs680001 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.fxs680001 to javafx.fxml;
-    exports com.example.fxs680001;
-}
